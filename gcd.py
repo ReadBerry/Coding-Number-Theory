7faf64e5514f3f53ff01565e0505d377a8fe6eaa
Read <76538559+ReadBerry@users.noreply.github.com>
2022-07-04 13:42:44 -0500

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,14 +1,19 @@
+#--Uses Euclid's algorithm to find the greatest common denominator (greatest common factor), or GCD of two positive integers
+
 import math
 
 def get_gcd(a,b):
-    m = a
-    n = b
-    while not a==b:
-        if a>b:
-            a = a-b
-        else:
-            b = b-a
-    return a
+    m = abs(a)
+    n = abs(b)
+    try:
+        while not a==b:
+            if a>b:
+                a = a-b
+            else:
+                b = b-a
+        return a
+    except:
+        print("Did you enter two integers?")
 
 print("Enter two integers")
 
